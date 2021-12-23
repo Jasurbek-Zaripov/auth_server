5a63f0577385fd137b6ffce8afd61d975281ea01
Jasurbek-Zaripov <zaripovjasurbek2001@gmail.com>
2021-12-23 21:03:21 +0500

    user_id qushildi!

--- app.js
+++ app.js
@@ -1,176 +1,33 @@
-const dbModule = require('./src/module/db.module')
+const { new_user } = require('./src/controller/new_user')
+const { check_user } = require('./src/controller/check_user')
+const { get_users } = require('./src/controller/get_users')
+const { user_exit } = require('./src/controller/user_exit')
+const { get_todo } = require('./src/controller/get_todo')
+const { put_todo } = require('./src/controller/put_todo')
+const { new_todo } = require('./src/controller/new_todo')
+const { check_browser, CORS } = require('./src/middleware/midd')
+
 const express = require('express')
 const app = express()
+
 const PORT = process.env.PORT || 3000
-const HOST = 'localhost'
 
-app.use((req, res, next) => {
-  let hder = JSON.stringify(req.headers['user-agent'])
-  if (/(mozilla\/)|(opera\/)/i.test(hder)) {
-    console.log('yes')
-    next()
-  } else {
-    return res.json({ Uzur: 'kirgiza olmayman!' })
-  }
-})
-
-app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*')
-  res.setHeader(
-    'Access-Control-Allow-Methods',
-    'GET, POST, OPTIONS, PUT, PATCH, DELETE'
-  )
-  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type')
-  res.setHeader('Access-Control-Allow-Credentials', true)
-  next()
-})
-
+//middleware
+app.use(check_browser)
+app.use(CORS)
 app.use(express.json())
 
-app.post('/user/new', async (req, res) => {
-  try {
-    let users = await dbModule.get_user_in_db()
-    let { username, password, birth, gender } = req.body
-    if (!username || !password || !birth || !gender) {
-      return res.json({ ERROR: "malumotni to'ldiring!" })
-    }
-    if (users[username]) {
-      return res.json({ ERROR: "bu user ro'yxatda bor!" })
-    }
+//users
+app.post('/user/new', new_user)
+app.post('/user/check', check_user)
+app.get('/users', get_users)
+app.put('/user/exit', user_exit)
 
-    users[username] = {
-      password,
-      gender,
-      birth,
-      online: true,
-      todo_list: [],
-    }
-
-    await dbModule.write_user_to_db(users)
-    return res.json({ message: 'user qushildi!' })
-  } catch (xato) {
-    return res.json({ ERROR: 'Qandaydir xato (bilish shart emas!)' })
-  }
-})
-
-app.post('/user/check', async (req, res) => {
-  try {
-    let users = await dbModule.get_user_in_db()
-    let { username, password } = req.body
-
-    if (!users[username]) {
-      return res.json({ ERROR: 'user topilmadi!' })
-    }
-    if (users[username]['password'] != password) {
-      return res.json({ ERROR: 'password xato!' })
-    }
-    users[username]['online'] = true
-    await dbModule.write_user_to_db(users)
-    return res.json({ message: 'ok ' + username })
-  } catch (xato) {
-    return res.json({ ERROR: 'Qandaydir xato (bilish shart emas!)' })
-  }
-})
-
-app.get('/users', async (req, res) => {
-  try {
-    let users = await dbModule.get_user_in_db()
-
-    for (const user in users) {
-      if (Object.hasOwnProperty.call(users, user)) {
-        const obj = users[user]
-        if (!obj['online']) {
-          delete users[user]
-        }
-      }
-    }
-    return res.json(users)
-  } catch (xato) {
-    return res.json({ ERROR: 'Qandaydir xato (bilish shart emas!)' })
-  }
-})
-
-app.put('/user/exit', async (req, res) => {
-  try {
-    let users = await dbModule.get_user_in_db()
-    let { username } = req.query
-    if (!users[username]) {
-      return res.json({ ERROR: 'user topilmadi!' })
-    }
-    users[username]['online'] = false
-    await dbModule.write_user_to_db(users)
-    return res.json({ message: 'user offline' })
-  } catch (xato) {
-    return res.json({ ERROR: 'Qandaydir xato (bilish shart emas!)' })
-  }
-})
-
-app.get('/todo', async (req, res) => {
-  try {
-    let users = await dbModule.get_user_in_db()
-    let { username } = req.query
-    if (!users[username]) {
-      return res.json({ ERROR: 'user topilmadi!' })
-    }
-    res.json(users[username]['todo_list'])
-  } catch (xato) {
-    return res.json({ ERROR: 'Qandaydir xato (bilish shart emas!)' })
-  }
-})
-
-app.put('/todo', async (req, res) => {
-  try {
-    let users = await dbModule.get_user_in_db()
-    let { username, todoID, holat } = req.body
-    if (!username || !todoID || !holat) {
-      return res.json({ ERROR: "malumotni to'ldiring!" })
-    }
-    if (!users[username]) {
-      return res.json({ ERROR: 'user topilmadi' })
-    }
-    if (!users[username]['todo_list'][todoID]) {
-      return res.json({ ERROR: 'user bunday malumot mavjud emas!' })
-    }
-    users[username]['todo_list'][todoID]['holat'] = holat
-
-    await dbModule.write_user_to_db(users)
-    return res.json({ message: 'haloti uzgartirildi!' })
-  } catch (xato) {
-    return res.json({ ERROR: 'Qandaydir xato (bilish shart emas!)' })
-  }
-})
-
-app.post('/todo/new', async (req, res) => {
-  try {
-    let { username, title, todo } = req.body
-    if (!username || !title || !todo) {
-      return res.json({ ERROR: 'malumotni kiriting!' })
-    }
-
-    let users = await dbModule.get_user_in_db()
-    if (!users[username]) {
-      return res.json({ ERROR: 'user topilmadi' })
-    }
-    for (const t of users[username]['todo_list']) {
-      if (t['title'] == title) {
-        return res.json({ ERROR: 'bu todo mavjud!' })
-      }
-    }
-
-    users[username]['todo_list'].push({
-      title,
-      todo,
-      holat: 'qizil',
-      time: new Date().toLocaleTimeString(),
-    })
-
-    await dbModule.write_user_to_db(users)
-    return res.json({ message: users[username]['todo_list'].length - 1 })
-  } catch (xato) {
-    return res.json({ ERROR: 'Qandaydir xato (bilish shart emas!)' })
-  }
-})
+//todos
+app.get('/todo', get_todo)
+app.put('/todo', put_todo)
+app.post('/todo/new', new_todo)
 
 app.listen(PORT, () =>
-  console.log(`Backend server is running http://${HOST}:${PORT}`)
+  console.log(`Backend server is running http://localhost:${PORT}`)
 )
